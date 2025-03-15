4b7822c200bcb29d9bd92e1e7acf8efc1a414470
Aliaksei Makarevich <electron2686@gmail.com>
2025-03-15 19:10:21 +0300

    config_handle

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,19 @@ import (
 	"matchfinder"
 	"matchfinder/internal/auth"
 	"net/http"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	confPath = "C:\\Son_Alex\\GO_projects\\MatchFinder\\MatchFinder\\configs"
 )
 
 func main() {
 	router := http.NewServeMux()
+	if err := initConfig(); err != nil {
+		log.Fatalf("An error during config reading: %s\n", err.Error())
+	}
 
 	//-------------------------SERVICES------------------------------
 	authService := auth.NewAuthService()
@@ -21,10 +30,16 @@ func main() {
 	//-------------------------HANDLERS------------------------------
 
 	//--------------------------SERVER-------------------------------
-	srv := matchfinder.NewServer("8080", router)
+	srv := matchfinder.NewServer(viper.GetString("port"), router)
 	err := srv.Run()
 	if err != nil {
 		log.Printf("Some error during server starting: %s", err.Error())
 	}
 	//--------------------------SERVER-------------------------------
+}
+
+func initConfig() error {
+	viper.AddConfigPath(confPath)
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
